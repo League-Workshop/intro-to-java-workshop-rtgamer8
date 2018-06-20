aa3346c1a3b3b1952ae6c0e57fd2fc5b691a2fca
jennakimsd <league@WTS-iMac-11.attlocal.net>
2018-06-20 14:55:41 -0700

    day3
    worked on speaking and pop ups

---
+++ src/section3/OwnAdventure.java
@@ -0,0 +1,9 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+	int direction  = JOptionPane.showOptionDialog(null, "Which Way", "Direction", 0, JOptionPane.INFORMATION_MESSAGE, null, new String[] {"Left","Right","Forward" direction);
+}
+}
--- src/section3/SpeakAndSpell.java
+++ src/section3/SpeakAndSpell.java
@@ -9,16 +9,60 @@ public class SpeakAndSpell {
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak("spell mandlebrot");
 		// 2. Catch the user's answer in a String
-
+String word  = JOptionPane.showInputDialog("What is the anwser?");
 		// 3. If the user spelled the word correctly, speak "correct"
+if(word.equals("mandlebrot")) {
+speak("correct!");
+}
 
 		// 4. Otherwise say "wrong"
+else {
+	speak("wrong!");
+}
 
+//1. Use the speak method to say the word. "e.g. spell mandlebrot"
+speak("spell superman ");
+		// 2. Catch the user's answer in a String
+String word2  = JOptionPane.showInputDialog("What is the anwser?");
+		// 3. If the user spelled the word correctly, speak "correct"
+if(word2.equals("superman")) {
+speak("correct!");
+speak("wrong!");
+// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+speak("spell nonsense");
+		// 2. Catch the user's answer in a String
+String word3  = JOptionPane.showInputDialog("What is the anwser?");
+		// 3. If the user spelled the word correctly, speak "correct"
+if(word3.equals("nonsense")) {
+speak("correct!");
+// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+speak("spell ninja");
+		// 2. Catch the user's answer in a String
+String word4 = JOptionPane.showInputDialog("What is the anwser?");
+		// 3. If the user spelled the word correctly, speak "correct"
+if(word4.equals("ninja")) {
+speak("correct!");
+}
+}
+}	}
+
+
+
+
+
+
+
+
+
+
+
+
+	
 		// 5. repeat the process for other words
 		
-	}
+	
 
 	static void speak(String words) {
 		try {
@@ -29,6 +73,26 @@ public class SpeakAndSpell {
 		}
 	}
 
+	
+	
+	
+
 }
 
 
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
